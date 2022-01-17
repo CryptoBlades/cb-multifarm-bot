9cd53f177f63942d2e316f18329e865412048c9d
eD3ath <edeath1208@gmail.com>
2022-01-17 19:47:39 +0800

    tweaks

--- src/tasks/partner-updater.js
+++ src/tasks/partner-updater.js
@@ -8,7 +8,7 @@ const config = require('../app-config.json')
 const duration = 10800
 
 const task = async (chain, test = false) => {
-  logger('main', `Running price-updater in ${chain}`)
+  logger('main', `Running price-updater on ${chain}`)
   const skillPrice = await web3Helper.getSkillPrice(chain)
   const { onContract } = web3Helper.web3LoadBalancer(chain)
   await onContract('treasury', async (contract) => {
@@ -25,12 +25,12 @@ const task = async (chain, test = false) => {
         gas: web3Helper.getGasLimit(chain),
         gasPrice: web3Helper.toWei(web3Helper.getGasPrice(chain), 'gwei')
       }
-      logger('info', `Updating price for SKILL in ${chain}`)
+      logger('info', `Updating price for SKILL on ${chain}`)
       if (!test) {
         const tx = await web3Helper.sendTransaction(chain, skillOptions, process.env.PRIVATE_KEY)
-        logger('success', `SKILL price updated in ${chain}. TX: ${tx.transactionHash}`)
+        logger('success', `SKILL price updated on ${chain}. TX: ${tx.transactionHash}`)
       } else {
-        logger('success', `SKILL price updated in ${chain}. Price: ${skillPriceInEther}`)
+        logger('success', `SKILL price updated on ${chain}. Price: ${skillPriceInEther}`)
       }
     } catch (e) {
       if (e.message === 'No price change detected.') logger('warn', `Skipped updating SKILL price. Reason: ${e.message}`)
@@ -41,10 +41,11 @@ const task = async (chain, test = false) => {
     if (partnerIds.length > 0) {
       async.eachSeries(partnerIds, async id => {
         const partnerInfo = await contract.methods.getPartnerProject(id).call()
+        let priceInEther = null
         try {
           const price = await fetch(`https://api.coingecko.com/api/v3/simple/token_price/${config.chains[process.env.CHAIN_ENV][chain].COINGECKO_ID}?contract_addresses=${partnerInfo[3]}&vs_currencies=usd`).then((res) => res.json()).catch(() => 0)
           if (!price) throw Error(`Error retrieving price from coingecko. ${price}`)
-          const priceInEther = web3Helper.toWei((partnerInfo[2] === 'SKILL' ? Number(skillPrice) : price[partnerInfo[3].toLowerCase()].usd), 'ether')
+          priceInEther = web3Helper.toWei((partnerInfo[2] === 'SKILL' ? Number(skillPrice) : price[partnerInfo[3].toLowerCase()].usd), 'ether')
           const increase = web3Helper.getIncreasePercentage(partnerInfo[6], priceInEther)
           if (increase > 100 || increase < -50) throw Error(`Unusual ${(increase >= 0 ? 'increase' : 'decrease')} in price. ${increase}%`)
           if (increase === 0) throw Error('No price change detected.')
@@ -54,17 +55,17 @@ const task = async (chain, test = false) => {
             gas: web3Helper.getGasLimit(chain),
             gasPrice: web3Helper.toWei(web3Helper.getGasPrice(chain), 'gwei')
           }
-          logger('info', `Updating price for ${partnerInfo[1]} in ${chain}`)
+          logger('info', `Updating price for ${partnerInfo[1]} on ${chain}`)
           if (!test) {
             const tx = await web3Helper.sendTransaction(chain, options, process.env.PRIVATE_KEY)
-            logger('success', `${partnerInfo[1]} price updated in ${chain}. TX: ${tx.transactionHash}`)
+            logger('success', `${partnerInfo[1]} price updated on ${chain}. TX: ${tx.transactionHash}`)
             await web3Helper.sleep(3000)
           } else {
-            logger('success', `${partnerInfo[1]} price updated in ${chain}. Price: ${priceInEther}`)
+            logger('success', `${partnerInfo[1]} price updated on ${chain}. Price: ${priceInEther}`)
           }
         } catch (e) {
           if (e.message === 'No price change detected.') logger('warn', `Skipped updating ${partnerInfo[1]} price. Reason: ${e.message}`)
-          else logger('error', `Error updating ${partnerInfo[1]} price. Reason: ${e.message}`)
+          else logger('error', `Error updating ${partnerInfo[1]} price ${priceInEther} on ${chain}. Reason: ${e.message}`)
         }
       })
     }
